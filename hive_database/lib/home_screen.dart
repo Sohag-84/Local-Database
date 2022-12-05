5544ebe0f9a6a3a87ba8d9f7626b62ef5b72c13d
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-12-05 11:05:51 +0600

    initialized hive

---
+++ hive_database/lib/home_screen.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+ const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
