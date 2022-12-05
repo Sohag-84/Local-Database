22d77a905a8bb1a4298d2d5762dd4a82c12a50a9
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-12-05 14:35:44 +0600

    create notes model class

---
+++ notes_app/lib/models/notes_model.dart
@@ -0,0 +1,13 @@
+import 'package:hive/hive.dart';
+part 'notes_model.g.dart';
+
+@HiveType(typeId: 0)
+class NotesModel {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  String description;
+
+  NotesModel({required this.title, required this.description});
+}
